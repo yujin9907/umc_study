5b19fb2ec2def6698523d51fc88d1c5309289a0f
choikyungsoo <ryan1954@naver.com>
2025-04-30 18:54:34 +0900

    FEAT : 5주차 워크북 내용 entity 추가

---
+++ 수_최경수/src/main/java/umc/spring/domain/FoodCategory.java
@@ -0,0 +1,20 @@
+package umc.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import umc.spring.domain.common.BaseEntity;
+
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class FoodCategory extends BaseEntity {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String name;
+
+}
